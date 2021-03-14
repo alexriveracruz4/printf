82830178fe66e339aa7976c239bd6cdf3a320b8c
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-14 21:20:33 +0000

    adding function S

--- print_string.c
+++ print_string.c
@@ -22,3 +22,58 @@ int print_s(va_list s)
 	}
 	return (i);
 }
+
+/**
+ * char_hex - print a ascci char in uppercase hexidecimal
+ * @ch: char tu print
+ *
+ * Return: cumber of printed chars
+ */
+
+int char_hex(char ch)
+{
+	int n;
+	char h[2];
+	char dif = 'A' - ':';
+
+	h[0] = ch / 16;
+	h[1] = ch % 16;
+	for (n = 0; n < 2; n++)
+	{
+		if (h[n] >= 10)
+			_putchar(h[n] + '0' + dif);
+		else
+			_putchar(h[n] + '0');
+	}
+	return (n);
+}
+/**
+ * print_S - print a string with the non printable characters (ASCII)
+ * @S: string to print
+ *
+ * Return: number of printed chars
+ */
+int print_S(va_list S)
+{
+	char *str = va_arg(S, char *);
+	int i, n = 0;
+
+	if (str == NULL)
+		str = "null";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			n += 2;
+			n += char_hex(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			n++;
+		}
+	}
+	return (n);
+}
